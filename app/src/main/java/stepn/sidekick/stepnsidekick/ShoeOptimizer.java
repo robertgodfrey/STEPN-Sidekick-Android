152628d73fda2a1792d0d468396dbae797e4e119
Rob <rob_godfrey@outlook.com>
2022-06-28 10:11:03 -1000

    Assigned all UI elements in Java

--- app/src/main/java/stepn/sidekick/stepnsidekick/ShoeOptimizer.java
+++ app/src/main/java/stepn/sidekick/stepnsidekick/ShoeOptimizer.java
@@ -2,13 +2,209 @@ package stepn.sidekick.stepnsidekick;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+/**
+ * Shoe optimizer. Uses community data to determine best points for GST earning and mystery box
+ * chance.
+ *
+ * @author Bob Godfrey
+ * @version 1.3.0
+ *
+ */
 
 public class ShoeOptimizer extends AppCompatActivity {
+
+    ImageButton shoeRarityButton, shoeTypeButton, optimizeButton;
+    SeekBar levelSeekbar;
+    EditText energyEditText, effEditText, luckEditText, comfortEditText, resEditText;
+
+    TextView shoeRarityTextView, shoeTypeTextView, levelTextView, effTotalTextView, luckTotalTextView,
+            comfortTotalTextView, resTotalTextView, pointsAvailableTextView, gstEarnedTextView,
+            gstLimitTextView, durabilityLossTextView, repairCostTextView, gstTotalTextView,
+            effMinusTv, effPlusTv, luckMinusTv, luckPlusTv, comfMinusTv, comfPlusTv, resMinusTv,
+            resPlusTv, optimizeTextView;
+
+    ImageView gemSocketOne, gemSocketOneShadow, gemSocketOneLockPlus, gemSocketTwo,
+            gemSocketTwoShadow, gemSocketTwoLockPlus, gemSocketThree, gemSocketThreeShadow,
+            gemSocketThreeLockPlus, gemSocketFour, gemSocketFourShadow, gemSocketFourLockPlus,
+            shoeTypeImageView, shoeCircles, shoeRarityButtonShadow, shoeTypeButtonShadow, minLevelImageView,
+            optimizeButtonShadow, mysteryBox1, mysteryBox2, mysteryBox3, mysteryBox4, mysteryBox5,
+            mysteryBox6, mysteryBox7, mysteryBox8, mysteryBox9, mysteryBox10, footOne, footTwo,
+            footThree;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shoe_optimizer);
+
+        buildUI();
+    }
+
+    // initializes all UI objects
+    @SuppressLint("ClickableViewAccessibility")
+    private void buildUI() {
+        shoeRarityButton = findViewById(R.id.shoeRarityButton);
+        shoeTypeButton = findViewById(R.id.shoeTypeButton);
+        optimizeButton = findViewById(R.id.optimizeButton);
+
+        levelSeekbar = findViewById(R.id.levelSeekBar);
+
+        energyEditText = findViewById(R.id.energyToSpendOptimizerEditText);
+        effEditText = findViewById(R.id.baseEffEditText);
+        luckEditText = findViewById(R.id.baseLuckEditText);
+        comfortEditText = findViewById(R.id.baseComfortEditText);
+        resEditText = findViewById(R.id.baseResEditText);
+
+        shoeRarityTextView = findViewById(R.id.shoeRarityTextView);
+        shoeTypeTextView = findViewById(R.id.shoeTypeTextView);
+        levelTextView = findViewById(R.id.levelTextView);
+        effTotalTextView = findViewById(R.id.totalEffTextView);
+        luckTotalTextView = findViewById(R.id.totalLuckTextView);
+        comfortTotalTextView = findViewById(R.id.totalComfTextView);
+        resTotalTextView = findViewById(R.id.totalResTextView);
+        pointsAvailableTextView = findViewById(R.id.pointsTextView);
+        gstEarnedTextView = findViewById(R.id.gstPerDayTextView);
+        gstLimitTextView = findViewById(R.id.gstLimitPerDayTextView);
+        durabilityLossTextView = findViewById(R.id.durabilityLossTextView);
+        repairCostTextView = findViewById(R.id.repairCostTextView);
+        gstTotalTextView = findViewById(R.id.gstIncomeTextView);
+        optimizeTextView = findViewById(R.id.optimizeTextView);
+
+        effMinusTv = findViewById(R.id.subEffTextView);
+        effPlusTv = findViewById(R.id.addEffTextView);
+        luckMinusTv = findViewById(R.id.subLuckTextView);
+        luckPlusTv = findViewById(R.id.addLuckTextView);
+        comfMinusTv = findViewById(R.id.subComfTextView);
+        comfPlusTv = findViewById(R.id.addComfTextView);
+        resMinusTv = findViewById(R.id.subResTextView);
+        resPlusTv = findViewById(R.id.addResTextView);
+
+        gemSocketOne = findViewById(R.id.gemSocketOne);
+        gemSocketOneShadow = findViewById(R.id.gemSocketOneShadow);
+        gemSocketOneLockPlus = findViewById(R.id.gemSocketOneLockPlus);
+        gemSocketTwo = findViewById(R.id.gemSocketTwo);
+        gemSocketTwoShadow = findViewById(R.id.gemSocketTwoShadow);
+        gemSocketTwoLockPlus = findViewById(R.id.gemSocketTwoLockPlus);
+        gemSocketThree = findViewById(R.id.gemSocketThree);
+        gemSocketThreeShadow = findViewById(R.id.gemSocketThreeShadow);
+        gemSocketThreeLockPlus = findViewById(R.id.gemSocketThreeLockPlus);
+        gemSocketFour = findViewById(R.id.gemSocketFour);
+        gemSocketFourShadow = findViewById(R.id.gemSocketFourShadow);
+        gemSocketFourLockPlus = findViewById(R.id.gemSocketFourLockPlus);
+
+        shoeTypeImageView = findViewById(R.id.shoeTypeImageView);
+        shoeCircles = findViewById(R.id.shoeBackground);
+        shoeRarityButtonShadow = findViewById(R.id.shoeRarityBoxShadow);
+        shoeTypeButtonShadow = findViewById(R.id.shoeTypeBoxShadow);
+        minLevelImageView = findViewById(R.id.seekbarMinLevel);
+        optimizeButtonShadow = findViewById(R.id.optimizeButtonShadow);
+
+        mysteryBox1 = findViewById(R.id.mysteryBoxLvl1);
+        mysteryBox2 = findViewById(R.id.mysteryBoxLvl2);
+        mysteryBox3 = findViewById(R.id.mysteryBoxLvl3);
+        mysteryBox4 = findViewById(R.id.mysteryBoxLvl4);
+        mysteryBox5 = findViewById(R.id.mysteryBoxLvl5);
+        mysteryBox6 = findViewById(R.id.mysteryBoxLvl6);
+        mysteryBox7 = findViewById(R.id.mysteryBoxLvl7);
+        mysteryBox8 = findViewById(R.id.mysteryBoxLvl8);
+        mysteryBox9 = findViewById(R.id.mysteryBoxLvl9);
+        mysteryBox10 = findViewById(R.id.mysteryBoxLvl10);
+
+        footOne = findViewById(R.id.footprint1ImageView);
+        footTwo = findViewById(R.id.footprint2ImageView);
+        footThree = findViewById(R.id.footprint3ImageView);
+
+        shoeRarityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO
+            }
+        });
+
+        shoeRarityButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                // TODO
+                return false;
+            }
+        });
+
+        shoeTypeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO
+            }
+        });
+
+        shoeTypeButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                // TODO
+                return false;
+            }
+        });
+
+        optimizeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO
+            }
+        });
+
+        optimizeButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        optimizeButton.setVisibility(View.INVISIBLE);
+                        optimizeTextView.setVisibility(View.INVISIBLE);
+                        optimizeButtonShadow.setImageResource(R.drawable.start_button);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        optimizeButton.setVisibility(View.VISIBLE);
+                        optimizeTextView.setVisibility(View.VISIBLE);
+                        optimizeButtonShadow.setImageResource(R.drawable.start_button_shadow);
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    // updates number of footprints depending on the type of shoe selected
+    private void updateFeet(int numFeet) {
+
+        switch (numFeet) {
+            case 1:
+                footOne.setImageResource(R.mipmap.footprint);
+                footTwo.setVisibility(View.GONE);
+                break;
+            case 2:
+                footTwo.setVisibility(View.VISIBLE);
+                footThree.setVisibility(View.GONE);
+                break;
+            case 3:
+                footOne.setImageResource(R.mipmap.footprint);
+                footTwo.setVisibility(View.VISIBLE);
+                footThree.setVisibility(View.VISIBLE);
+                break;
+            case 4:
+                footOne.setImageResource(R.mipmap.trainer_t);
+                footTwo.setVisibility(View.GONE);
+                footThree.setVisibility(View.GONE);
+                break;
+            default:
+                footOne.setImageResource(R.mipmap.bolt);
+        }
     }
 }
