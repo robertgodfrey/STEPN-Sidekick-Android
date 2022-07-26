eb919b8d54c4a2b254e363f90733af5b94c60f3d
Rob <rob_godfrey@outlook.com>
2022-07-25 15:35:29 -1000

    Set up layout for shoe name

--- app/src/main/java/stepn/sidekick/stepnsidekick/MainActivity.java
+++ app/src/main/java/stepn/sidekick/stepnsidekick/MainActivity.java
@@ -65,9 +65,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
